7634f7d846b6482051aae303eac4562764c59607
Vidhi Nimavat <vhnimavat2004@gmail.com>
2026-02-15 14:38:01 +0530

    Delete prime.py

--- prime.py
+++
@@ -1,9 +0,0 @@
-#WAP to print prime number series between 1 to 50
-
-for num in range(2, 51):
-    count = 0
-    for i in range(1, num + 1):
-        if num % i == 0:
-            count += 1
-    if count == 2:
-        print(num, end=" ")
