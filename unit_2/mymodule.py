e42ddd58406957586e4504cf2c005ea136845b3c
Vidhi Nimavat <vhnimavat2004@gmail.com>
2026-04-14 10:37:18 +0530

    Add files via upload

---
+++ unit_2/mymodule.py
@@ -0,0 +1,16 @@
+#Create a module with 4 functions of your choice. Import and use the functions using module in different ways.
+
+def add(a,b):
+    return a+b
+
+def subtract(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    if b==0:
+        return "Divide by zero is not possible."
+    return a/b
+        
